fe602dedd0af5a2742186f9f9c2b7936e427fe44
Erik Dillingham <erik.dillingham@gmail.com>
2016-06-13 08:20:19 -0700

    added "role" column to server db sql

--- ec2info.sql
+++ ec2info.sql
@@ -4,5 +4,6 @@ CREATE TABLE Servers (
     instance_id VARCHAR(32),
     private_ip_address VARCHAR(16),
     name VARCHAR(128),
-    key_name VARCHAR(64)
+    key_name VARCHAR(64),
+    role VARCHAR(16)
 );
