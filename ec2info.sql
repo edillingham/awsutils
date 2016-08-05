eae6a3de147c623b88997b303cdd651caa1828f6
Erik Dillingham <erik.dillingham@gmail.com>
2016-08-04 22:27:07 -0700

    shortened column name

--- ec2info.sql
+++ ec2info.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS Servers;
 
 CREATE TABLE Servers (
     instance_id VARCHAR(32),
-    private_ip_address VARCHAR(16),
+    private_ip VARCHAR(16),
     name VARCHAR(128),
     key_name VARCHAR(64),
     role VARCHAR(16)
